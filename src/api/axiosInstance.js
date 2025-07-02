a42f12c02bc3f51df5033ae47882a5137f4d39eb
Vishnu-raghav <vishnur1225@gmail.com>
2025-07-02 23:57:12 +0530

    fix error

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://school-website-backend-ybs4.onrender.com/api/v1",
   withCredentials: true,
 });
 
