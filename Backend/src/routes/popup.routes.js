d2d9262ddb9e86dbe8cfd2eeec041d9d747151ee
Vishnu Raghav <vishnur1225@gmail.com>
2025-07-09 04:14:12 -0700

    popup error fix

--- Backend/src/routes/popup.routes.js
+++ Backend/src/routes/popup.routes.js
@@ -10,13 +10,16 @@ import { singleUpload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-
+// Upload pop-up image (admin only)
 router.post("/upload", verifyJWT, singleUpload("image"), uploadPopupImage);
 
+// Delete image (admin only)
 router.delete("/:id", verifyJWT, deletePopupImage);
 
+// Toggle pop-up active/inactive (admin only)
 router.patch("/toggle/:id", verifyJWT, togglePopupStatus);
 
+// Get all pop-up images (public)
 router.get("/", getAllPopupImages);
 
 export default router;
